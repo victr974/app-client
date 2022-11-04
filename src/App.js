0859da1054161898f9065bfe89c9cc58865744f5
Victor Cortez <vcortezs@miumg.edu.gt>
2022-11-04 00:08:05 -0600

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world whit React
+          Hello world whit React and test with heroku 
         </p>
         <a
           className="App-link"
