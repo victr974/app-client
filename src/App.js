3b533b3bcb6d21225c274c9c3904176bb6e89ae8
Victor Cortez <vcortezs@miumg.edu.gt>
2022-11-04 09:42:01 -0600

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList'
 import Menu from './components/Navbar'
-
 import { Container } from '@mui/material'
 
 
